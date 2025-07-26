e44e6c2ccd029a0c113fae306fd5ee1008a21bb6
Cristian brogni <cristianbrogni_28@hotmail.com>
2025-07-25 22:28:01 -0300

    feat: adicinado barra de progresso com cores dinamicas por porcentagem

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -12,10 +12,28 @@ export default function TaskList({ tasks, onToggleSubTask}: Props) {
 
     return (
         <div className="mt-6 space-y-4">
-            {tasks.map((task) => (
+            {tasks.map((task) => {
+                const total = task.subtasks.length;
+                const completed = task.subtasks.filter((s) => s.completed).length;
+                const porcentage =  Math.round((completed / total) * 100);
+
+                return (
                 <div key={task.id} className="border p-4 rounded shadow">
                     <h3 className="text-lg font-bold mb-1">{task.name}</h3>
                     <p className="text-sm text-gray-600">Total: {task.total} | Divisão: {task.division}</p>
+                    {/* Progress Bar */}
+                    <div className="w-full bg-gray-200 h-3 rounded overflow-hidden mb-2">
+                        <div className={`h-full transmission-all duration-300 ${porcentage === 100 
+                            ? "bg-green-500" 
+                            : porcentage >= 67 
+                            ? "bg-blue-500" 
+                            : porcentage >=34 
+                            ? "bg-yellow-500" 
+                            : "bg-red-500"
+                        }`} 
+                        style={{width: `${porcentage}%`}}></div>
+                    </div>
+
                     <ul className="mt-2 text-sm list-disc list-inside">
                         {task.subtasks.map((subtask, i) => (
                             <li key={i} className="flex items-center gap-2">
@@ -29,7 +47,7 @@ export default function TaskList({ tasks, onToggleSubTask}: Props) {
                         ))}
                     </ul>
                 </div>
-            ))}
+            )})}
         </div>
     )
 }
